bed9036c10b6263ddb16cf70ebe894936b6fcda6
rafffel <rafffel1@gmail.com>
2017-11-07 19:44:48 +0100

    added settings
    added:
    toggle name button
    name offset y
    fov
    scale text factor

--- vrnames.py
+++ vrnames.py
@@ -17,27 +17,47 @@ initialLabelText = "Welcome to VR-Names."
 driverInformation = []
 labelStorage = []
 
-
+show_name = 0
 spinner_y_offset = 0
-
+scale_factor = 0
+fov_setting = 0
 def acMain(ac_version):
-    global appWindow, labelStorage, spinner_y_offset
+    global appWindow, labelStorage, spinner_y_offset, scale_factor, fov_setting
 
     appWindow=ac.newApp("VR-Names")
     ac.setTitle(appWindow, "VR-names")
-    ac.setSize(appWindow, 200, 80)
+    ac.setSize(appWindow, 200, 200)
     ac.drawBorder(appWindow,0)
     ac.setBackgroundOpacity(appWindow, 0)
 
     # ac.setIconPosition(appWindow, -9000, 0)
 
 
-    spinner_y_offset = ac.addSpinner(appWindow,"NameOffset")
-    ac.setRange( spinner_y_offset, -100, 100)
+    spinner_y_offset = ac.addSpinner(appWindow, "Name Height Offset")
+    ac.setRange(spinner_y_offset, -100, 100)
     ac.setValue(spinner_y_offset, 0)
-    ac.setStep(spinner_y_offset,1)
-    ac.setPosition(spinner_y_offset,10, 100)
+    ac.setStep(spinner_y_offset, 1)
+    ac.setPosition(spinner_y_offset, 10, 90)
+    ac.setSize(spinner_y_offset, 180, 20)
 
+    scale_factor = ac.addSpinner(appWindow, "scale text factor")
+    ac.setRange(scale_factor, 1, 30)
+    ac.setValue(scale_factor, 10)
+    ac.setStep(scale_factor, 1)
+    ac.setPosition(scale_factor, 10, 135)
+    ac.setSize(scale_factor, 180, 20)
+
+    fov_setting = ac.addSpinner(appWindow, "scale text factor")
+    ac.setRange(fov_setting, 0, 360)
+    ac.setValue(fov_setting, 110)
+    ac.setStep(fov_setting, 1)
+    ac.setPosition(fov_setting, 10, 180)
+    ac.setSize(fov_setting, 180, 20)
+
+    button_toggle_name = ac.addButton(appWindow,"Toggle name")
+    ac.addOnClickedListener(button_toggle_name, toggle_name)
+    ac.setSize(button_toggle_name,180,20)
+    ac.setPosition(button_toggle_name, 10, 40)
 
     for x in range(ac.getCarsCount()):
         label = ac.addLabel(appWindow, "")
@@ -46,25 +66,36 @@ def acMain(ac_version):
 
     setInitialLabel()
     return "vrnames"
-
+def toggle_name(v1,v2):
+    global show_name
+    if show_name == 0:
+        show_name = 1
+    else:
+        show_name = 0
 def acUpdate(deltaT):
-    global appWindow, windowOpacity,appPosX,appPosY
-    appPosX,appPosY = ac.getPosition(appWindow)
-    ac.setBackgroundOpacity(appWindow, windowOpacity)
-    detectionArea = getDetectionArea()
-    #ac.console(detectionArea)
-    getDriverInformation(detectionArea)
-
+    global appWindow, windowOpacity,appPosX,appPosY,labelStorage,show_name
+    if show_name == 1:
+        appPosX,appPosY = ac.getPosition(appWindow)
+        ac.setBackgroundOpacity(appWindow, windowOpacity)
+        detectionArea = getDetectionArea()
+        #ac.console(detectionArea)
+        getDriverInformation(detectionArea)
+    else:
+        for x in range(ac.getCarsCount()):
+            ac.setPosition(labelStorage[x], 0, 0)
+            ac.setText(labelStorage[x], "")
 def getDetectionArea():
+    global fov_setting
     posX, posZ, posY = ac.getCarState(0, acsys.CS.WorldPosition)
     rads = getRotation(0)
-    PointTopLeftX, PointTopLeftY = move_vector((posX, posY + 200), rads - math.radians(55), (posX, posY))
-    PointTopRightX, PointTopRightY = move_vector((posX, posY + 200), rads + math.radians(55), (posX, posY))
+    fov_angle = ac.getValue(fov_setting)
+    PointTopLeftX, PointTopLeftY = move_vector((posX, posY + 200), rads - math.radians(fov_angle/2), (posX, posY))
+    PointTopRightX, PointTopRightY = move_vector((posX, posY + 200), rads + math.radians(fov_angle/2), (posX, posY))
 
     return ((posX, posY), (PointTopLeftX, PointTopLeftY), (PointTopRightX, PointTopRightY))
 
 def getDriverInformation(detectionArea):
-    global labelStorage,appPosX,appPosY,spinner_y_offset
+    global labelStorage,appPosX,appPosY,spinner_y_offset,fov_setting
     triangle = Triangle(detectionArea[0], detectionArea[1], detectionArea[2])
     setLabel = 0
     for x in range(ac.getCarsCount()):
@@ -75,10 +106,11 @@ def getDriverInformation(detectionArea):
             distance = math.sqrt(math.pow(vect_x,2) + math.pow(vect_y,2))
             newPosition = getRenderPosition(x, detectionArea, (posX, posY))
             ac.setText(labelStorage[setLabel], ac.getDriverName(x))
-            xPos = (((newPosition * windowSizeX) / 110) - appPosX)
+            fov_angle = ac.getValue(fov_setting)
+            xPos = (((newPosition * windowSizeX) / fov_angle) - appPosX)
             yOffset = ac.getValue(spinner_y_offset)
             yPos = (((windowSizeY / 2) - 20) - appPosY) + int(yOffset)
-            fontSize = 10 * (1 / (distance / 100))
+            fontSize = (10 * (1 / (distance / 100))) * (ac.getValue(scale_factor)/10)
             ac.setPosition(labelStorage[setLabel], xPos, yPos)
             ac.setFontSize(labelStorage[setLabel], fontSize)
             setLabel += 1
